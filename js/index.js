0d59c6b6107210cff9e5b2766e8035223b31aed9
Joeet33 <up931673@myport.ac.uk>
2020-05-09 16:30:51 +0100

    alert removed

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@
 
 $(document).ready(function(){
 
-alert("We use cookies on this site to enhance your user experience.\nFor a complete overview of all cookies used, please see your personal settings.");
-    
 $("#flex-item_1").click(function(){
    
   //alert("Page 1 Button working");
